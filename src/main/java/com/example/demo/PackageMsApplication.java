5b796f67634ecba3752054f1776ee85e07f0b6ed
skundu <shiladitya.kundu@capgemini.com>
2020-05-15 10:16:05 +0530

    initial commit

---
+++ src/main/java/com/example/demo/PackageMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageMsApplication.class, args);
+	}
+
+}
